414703084cb792e62c3377933ff538996265f644
perinze <perinzefaper@foxmail.com>
2023-04-10 17:45:00 +0800

    rename gallery to gov_header

--- app/src/main/java/com/perinze/merge/ui/gov_header/GovHeaderFragment.kt
+++ app/src/main/java/com/perinze/merge/ui/gov_header/GovHeaderFragment.kt
@@ -1,4 +1,4 @@
-package com.perinze.merge.ui.gallery
+package com.perinze.merge.ui.gov_header
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.perinze.merge.databinding.FragmentGalleryBinding
+import com.perinze.merge.databinding.FragmentGovHeaderBinding
 
-class GalleryFragment : Fragment() {
+class GovHeaderFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentGovHeaderBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,12 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val govHeaderViewModel =
+            ViewModelProvider(this).get(GovHeaderViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentGovHeaderBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
---
+++ app/src/main/java/com/perinze/merge/ui/gov_header/GovHeaderViewModel.kt
@@ -0,0 +1,9 @@
+package com.perinze.merge.ui.gov_header
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GovHeaderViewModel : ViewModel() {
+
+}
