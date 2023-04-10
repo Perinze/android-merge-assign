95afe2bc54e9e1d7cab981e42adba4261f3f42aa
perinze <perinzefaper@foxmail.com>
2023-04-10 17:52:54 +0800

    add gov files

---
+++ app/src/main/java/com/perinze/merge/ui/gov_header/GovHeader.kt
@@ -0,0 +1,3 @@
+package com.perinze.merge.ui.gov_header
+
+data class GovHeader(val title: String, val img: String, val url: String)
---
+++ app/src/main/java/com/perinze/merge/ui/gov_header/GovHeaderAdapter.kt
@@ -0,0 +1,49 @@
+package com.perinze.merge.ui.gov_header
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.perinze.merge.R
+
+class GovHeaderAdapter(private val context: Context, private val lifecycleOwner: LifecycleOwner, private val liveData: LiveData<List<GovHeader>>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    init {
+        liveData.observe(lifecycleOwner) {
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView: View = LayoutInflater.from(context).inflate(R.layout.fragment_gov_header, parent, false)
+        return GovHeaderHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return liveData.value!!.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val govHeaderHolder = holder as GovHeaderHolder
+        govHeaderHolder.imageView.load(liveData.value!![position].img)
+        govHeaderHolder.textView.text = liveData.value!![position].title
+        govHeaderHolder.itemView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(liveData.value!![position].url))
+            context.startActivity(intent)
+        }
+    }
+
+    class GovHeaderHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.govHeaderItemImageView)
+        val textView: TextView = itemView.findViewById(R.id.govHeaderItemTextView)
+    }
+}
