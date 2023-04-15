96da24e4d780d415c8c12b7f3543c4a5a398b476
perinze <perinzefaper@foxmail.com>
2023-04-15 20:41:26 +0800

    fix a little

--- app/src/main/java/com/perinze/merge/SearchActivity.kt
+++ app/src/main/java/com/perinze/merge/SearchActivity.kt
@@ -1,31 +1,22 @@
 package com.perinze.merge
 
-import android.app.Activity
 import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
-import android.widget.SearchView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStore
-import androidx.lifecycle.ViewModelStoreOwner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.perinze.merge.ui.search.SearchResult
+import com.google.android.material.appbar.AppBarLayout
 import com.perinze.merge.ui.search.SearchResultAdapter
 import com.perinze.merge.ui.search.SearchViewModel
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var root: ConstraintLayout
     private lateinit var searchViewModel: SearchViewModel
-    private lateinit var searchResultSearchView: SearchView
     private lateinit var searchResultRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +25,6 @@ class SearchActivity : AppCompatActivity() {
 
         setContentView(R.layout.search_result)
         root = findViewById(R.id.search_result_view)
-        searchResultSearchView = root.findViewById(R.id.search_result_search_view)
         searchResultRecyclerView = root.findViewById(R.id.search_result_recycler_view)
 
         val linearLayoutManager = LinearLayoutManager(this)
