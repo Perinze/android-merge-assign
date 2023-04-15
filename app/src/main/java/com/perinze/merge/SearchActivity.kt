4fca12ae2b20ca5320f017e8a999ac1d02a3bda9
perinze <perinzefaper@foxmail.com>
2023-04-15 20:32:31 +0800

    functional search

--- app/src/main/java/com/perinze/merge/SearchActivity.kt
+++ app/src/main/java/com/perinze/merge/SearchActivity.kt
@@ -6,20 +6,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.perinze.merge.ui.search.SearchResult
+import com.perinze.merge.ui.search.SearchResultAdapter
 import com.perinze.merge.ui.search.SearchViewModel
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 class SearchActivity : AppCompatActivity() {
-    private lateinit var root: TextView
+    private lateinit var root: ConstraintLayout
     private lateinit var searchViewModel: SearchViewModel
+    private lateinit var searchResultSearchView: SearchView
+    private lateinit var searchResultRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("search activity", "onCreate")
@@ -27,6 +34,14 @@ class SearchActivity : AppCompatActivity() {
 
         setContentView(R.layout.search_result)
         root = findViewById(R.id.search_result_view)
+        searchResultSearchView = root.findViewById(R.id.search_result_search_view)
+        searchResultRecyclerView = root.findViewById(R.id.search_result_recycler_view)
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        searchResultRecyclerView.layoutManager = linearLayoutManager
+
+        searchResultRecyclerView.adapter = SearchResultAdapter(this, this, searchViewModel.result)
 
         handleIntent(intent)
     }
---
+++ app/src/main/java/com/perinze/merge/ui/search/SearchResultAdapter.kt
@@ -0,0 +1,53 @@
+package com.perinze.merge.ui.search
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.perinze.merge.R
+
+class SearchResultAdapter(private val context: Context, private val lifecycleOwner: LifecycleOwner, private val liveData: LiveData<List<SearchResult>>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    init {
+        liveData.observe(lifecycleOwner) {
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView: View = LayoutInflater.from(context).inflate(R.layout.search_result_item, parent, false)
+        return SearchResultHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return liveData.value!!.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val searchResultHolder = holder as SearchResultHolder
+        searchResultHolder.titleView.text = liveData.value!![position].title
+        searchResultHolder.previewView.text = liveData.value!![position].preview
+        searchResultHolder.groupView.text = liveData.value!![position].group
+        searchResultHolder.dateView.text = liveData.value!![position].date
+        searchResultHolder.itemView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(liveData.value!![position].link))
+            context.startActivity(intent)
+        }
+    }
+
+    class SearchResultHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleView: TextView = itemView.findViewById(R.id.search_result_item_title)
+        val previewView: TextView = itemView.findViewById(R.id.search_result_item_preview)
+        val groupView: TextView = itemView.findViewById(R.id.search_result_item_group)
+        val dateView: TextView = itemView.findViewById(R.id.search_result_item_date)
+    }
+}
